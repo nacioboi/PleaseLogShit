dcbcdfb99bf00bc7a5c80d8a86e275a70922d4c6
Joel Watson <63216246+nacioboi@users.noreply.github.com>
2024-05-16 23:17:53 +1000

    made `templated_setup` work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from templated_setup import templated_setup
 
 templated_setup.Setup_Helper.init(".templated_setup.cache.json")
 templated_setup.Setup_Helper.setup(
-	"plsp",
-	"matrikater (Joel Watson)",
-	"A simple, easy to use, and powerful logging library for Python.",
+	name="plsp",
+	author="matrikater (Joel Watson)",
+	description="A simple, easy to use, and powerful logging library for Python.", 
 )
